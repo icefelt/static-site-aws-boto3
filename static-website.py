6757dcb25a31279762d60e13f722532274211b15
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-19 16:32:00 -0700

    configure bucket to be publicly accessible

--- static-website.py
+++ static-website.py
@@ -14,3 +14,22 @@ website_configuration = {
 }
 s3.put_bucket_website(Bucket=bucket_name, WebsiteConfiguration=website_configuration)
 
+# Upload the static website files to the bucket
+for file_name in ['index.html', '404.html']:
+    with open(file_name, 'rb') as f:
+        s3.upload_fileobj(f, bucket_name, file_name)
+
+# Configure the bucket to be publicly accessible
+s3.put_bucket_policy(
+    Bucket=bucket_name,
+    Policy=json.dumps({
+        'Version': '2012-10-17',
+        'Statement': [{
+            'Sid': 'PublicReadGetObject',
+            'Effect': 'Allow',
+            'Principal': '*',
+            'Action': 's3:GetObject',
+            'Resource': 'arn:aws:s3:::my-static-website/*'
+        }]
+    })
+)
